96618df708dcbbe81d23545973c9624e33be26e2
saish-p-code-82 <saish.p.code@gmail.com>
2025-07-24 15:07:03 +0530

    Update scriptLogin.js

--- scriptLogin.js
+++ scriptLogin.js
@@ -23,68 +23,26 @@ function validateForm() {
     {
         window.location.href = "form.html";
     return false;
-    }
-    else if(userId === "ALPHIA2025_02" && password === "KLS@8374")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-     else if(userId === "ALPHIA2025_03" && password ==="DP@6249")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-    else if(userId === "ALPHIA2025_04" && password ==="STP@5120")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-    else if(userId === "ALPHIA2025_05" && password ==="SG@2891")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-     else if(userId === "ALPHIA2025_06" && password ==="BB@6743")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-      else if(userId === "ALPHIA2025_07" && password ==="ANG@9340")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-       else if(userId === "ALPHIA2025_08" && password ==="PB@1912")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-        else if(userId === "ALPHIA2025_09" && password ==="LS@4538")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-         else if(userId === "	ALPHIA2025_10" && password ==="JH@8250")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-          else if(userId === "ALPHIA2025_11" && password ==="HERD@3801")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-           else if(userId === "ALPHIA2025_12" && password ==="BHA@2647")
-    {
-      window.location.herf = "form.html";
-    return false;
-    }
-    else
-    {
-        alert("Invalid User ID or Password.");
-        return false;
-    }
-  // Optional: Add more custom validation
-  return true; // proceed with form submission
+   
 }
+
+  // Disable right-click
+  document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+  });
+
+  // Disable Ctrl+U, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+S, F12
+  document.addEventListener('keydown', function (e) {
+    // F12
+    if (e.key === "F12") {
+      e.preventDefault();
+    }
+    // Ctrl+Shift+I or Ctrl+Shift+J or Ctrl+U or Ctrl+S
+    if ((e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
+        (e.ctrlKey && (e.key === 'U' || e.key === 'S'))) {
+      e.preventDefault();
+    }
+  });
+
+
 
